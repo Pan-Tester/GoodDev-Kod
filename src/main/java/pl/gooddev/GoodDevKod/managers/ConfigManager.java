0c619f1581ac05f02d256fd78e08d5441e5ed1f5
Pan-Tester <pan_tester@debil.ovh>
2025-03-10 19:01:22 +0100

    Update ConfigManager.java

--- src/main/java/pl/gooddev/GoodDevKod/managers/ConfigManager.java
+++ src/main/java/pl/gooddev/GoodDevKod/managers/ConfigManager.java
@@ -20,8 +20,3 @@
 /*    */   }
 /*    */ }
 
-
-/* Location:              C:\Users\Milos\IdeaProjects\GoodDev-Kod\target\GoodDev-GoodDevKod-1.0.jar!\pl\gooddev\GoodDevKod\managers\ConfigManager.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
